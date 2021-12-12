bf721df1ea848e991004e9545b37ce8a5f83df99
Jonathan Birkholz <rookieone@gmail.com>
2021-12-12 00:11:02 -0600

    Elixir solved day 4 part 1

--- rookieone/elixir/advent_of_code_2021/test/day4/part1_test.exs
+++ rookieone/elixir/advent_of_code_2021/test/day4/part1_test.exs
@@ -9,6 +9,14 @@ defmodule AdventOfCode2021.Day4.Part1Test do
 
     {:ok, answer} = solve(input)
 
-    assert answer.score == 4512
+    assert answer.score == 4_512
+  end
+
+  test "input should return answer" do
+    {:ok, input} = read_file("test/day4/input.txt")
+
+    {:ok, answer} = solve(input)
+
+    assert answer.score == 11_536
   end
 end
