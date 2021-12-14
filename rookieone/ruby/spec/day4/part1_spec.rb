5eb8cafc1512e4fb6586642ab2a1a47c1b6f086b
Jonathan Birkholz <rookieone@gmail.com>
2021-12-13 21:43:41 -0600

    Ruby solve day 4 part 1

--- rookieone/ruby/spec/day4/part1_spec.rb
+++ rookieone/ruby/spec/day4/part1_spec.rb
@@ -7,6 +7,14 @@ RSpec.describe "Day 4 Part 1" do
     
     answer = Day4Part1.solve(input)
 
-    p answer
+    expect(answer).to   eq(4512)
+  end
+
+  it "input should return answer" do
+    input = Day4FileReader.read_file("./spec/day4/input.txt")
+    
+    answer = Day4Part1.solve(input)
+
+    expect(answer).to   eq(11536)
   end
 end
