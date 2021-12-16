d830f2565bbe17fed91198f2e8e34618b14949e0
Patrick Kurth <patrickwkurth@gmail.com>
2021-12-15 23:05:05 -0600

    Started tinkering with the functionality.

--- pwkurth/node/day_3/problem3_part1.js
+++ pwkurth/node/day_3/problem3_part1.js
@@ -1,5 +1,36 @@
 function powerConsumption(input) {
-    return "Hey lol"
-}
 
+    let bit_1 = null
+    let bit_0 = null
+    let gamma_rate = []
+    let epislon_rate = []
+
+    input.forEach(element => {
+        console.log(element)
+        if (element[1].startsWith("1")) {
+
+            bit_1++
+
+            console.log(`The bit 1 is : ${bit_1}`)
+        }
+        if (element[2].startsWith("0")) {
+
+            bit_0++
+            console.log(`The bit 0 is : ${bit_0}`)
+        }
+    })
+    if (bit_1 > bit_0) {
+
+        gamma_rate += 1
+        console.log(`bit_1 total is: ${bit_1}`)
+        console.log(gamma_rate)
+    }
+    if (bit_0 < bit_1) {
+        epislon_rate += 0
+        console.log(`bit_0 total is: ${bit_0}`)
+        console.log(epislon_rate)
+
+    }
+    return zero
+}
 exports.powerConsumption = powerConsumption
--- pwkurth/node/day_3/readPuzzle.js
+++ pwkurth/node/day_3/readPuzzle.js
@@ -4,11 +4,9 @@ export const readPuzzle = filepath => {
 
     const input = fs.readFileSync(filepath).toString().split("\r\n")
 
-    const result_arr = input.map(function (x) {
-        return parseInt(x, 10);
-    })
-
-    console.log(result_arr)
+    // const result_arr = input.map(function (x) {
+    //     return parseInt(x, 10);
+    // })
 
     console.log(input)
     return input;
